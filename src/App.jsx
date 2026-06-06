15176bb8dccccdb03f91a9f84a6a5f873d0898e0
KarinaAdame <shaddayadams@gmail.com>
2026-06-06 18:21:11 -0400

    Se me olvido guardar la parte para que aparezca el inventario jajaja

--- src/App.jsx
+++ src/App.jsx
@@ -70,6 +70,7 @@ function App() {
         <Route path="contabilidad/cuentas-por-cobrar" element={<RoleRoute allowedRoles={["master", "admin"]}><AccountsReceivable /></RoleRoute>}/>
         <Route path="contabilidad/cuentas-por-pagar" element={<RoleRoute allowedRoles={["master", "admin"]}><AccountsPayable /></RoleRoute>}/>
         <Route path="configuracion/cuenta" element={<RoleRoute allowedRoles={["master"]}><AccountSettings /></RoleRoute>}/>
+        <Route path="conteo-inventario" element={<RoleRoute allowedRoles={["master", "admin"]}><div>Conteo de inventario</div></RoleRoute>}/>
       
       </Route>
 
